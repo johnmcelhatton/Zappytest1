dfb4033580c5bde7b308fd7f2ff52046201898cb
John McElhatton <jmcelhatton2010@gmail.com>
2024-01-16 15:17:01 -0500

    Initial Commit

---
+++ Zappytest1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Zappytest1
+//
+//  Created by John McElhatton on 1/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Zappytest1/Zappytest1App.swift
@@ -0,0 +1,17 @@
+//
+//  Zappytest1App.swift
+//  Zappytest1
+//
+//  Created by John McElhatton on 1/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Zappytest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
